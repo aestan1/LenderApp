6b2c1fb7b72c23f45f41b0bd66c1dd07d1c0597c
AndresEstan <andrestan1995@gmail.com>
2017-11-12 18:33:40 -0500

    Agrega y muestra los usuarios

---
+++ app/src/main/java/com/example/estan/lenderapp/AdaptadorCliente.java
@@ -0,0 +1,8 @@
+package com.example.estan.lenderapp;
+
+/**
+ * Created by Estan on 12/11/2017.
+ */
+
+public class AdaptadorCliente {
+}
---
+++ app/src/main/java/com/example/estan/lenderapp/AgregarAbono.java
@@ -0,0 +1,13 @@
+package com.example.estan.lenderapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgregarAbono extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_abono);
+    }
+}
